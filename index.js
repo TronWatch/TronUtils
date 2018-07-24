1236e67909a13df8b64369df256bf82390a4a2eb
metjm <metjm.mail@gmail.com>
2018-07-25 01:10:19 +0200

    add soliditywallet/getaccount

--- index.js
+++ index.js
@@ -9,15 +9,17 @@ module.exports = {
 };
 
 /*
-  async function test(){
+async function test() {
     let rpc = new module.exports.rpc();
 
     let priv = "d5f10a95bc9205062724f8487c2857c509ac6de6ac45c86c4716d12dff1cdb5987f373741328230dcebff59199db90d633d58930832bcfe5409cac0535297dca";
     let recipient = "TFoQGhmSQzLgnZjLYGAgGkAH8GUoe8J4Qb";
 
-    let response = await rpc.sendTrx(priv, recipient, 5000);
-    console.log(response);
-  }
+    //let response = await rpc.sendTrx(priv, recipient, 5000);
 
-  test();
+    let response = await rpc.getAccount(recipient);
+    console.log(response);
+}
+
+test();
 */
--- rpc.js
+++ rpc.js
@@ -1,52 +1,67 @@
 const accounts = require("./account.js");
 
 const axios = require("axios");
-const {pubToHex, getSignature} = require("./utils/crypto");
+const { pubToHex, getSignature } = require("./utils/crypto");
 
-class Rpc{
+class Rpc {
 
-  constructor(config = {}){
-    this.url_full = config.url_full || "http://rpc.tron.watch:8090";
-    this.url_solidity = config.url_solidity || "http://rpc.tron.watch:8091";
-  }
+    constructor(config = {}) {
+        this.url_full = config.url_full || "http://rpc.tron.watch:8090";
+        this.url_solidity = config.url_solidity || "http://rpc.tron.watch:8091";
+    }
 
-  async rpcReq(url, args= {}){
-    return (await axios.post(this.url_full + url, args, {
-      headers: {
-        'content-type': 'application/json',
-      },
-    })).data;
-  }
+    async rpcReq(server, url, args = {}) {
+        return (await axios.post(server + url, args, {
+            headers: {
+                'content-type': 'application/json',
+            },
+        })).data;
+    }
 
-  signTransaction(privateKey, transaction){
-    transaction.signature = getSignature(privateKey, transaction.txID);
-    return transaction;
-  }
 
-  async broadcastTransaction(transaction){
-    return (await this.rpcReq("/wallet/broadcasttransaction", transaction)).data;
-  }
+    async fullReq(url, args={}){
+        return this.rpcReq(this.url_full, url, args);
+    }
+    async solidityReq(url, args={}){
+        return this.rpcReq(this.url_solidity, url, args);
+    }
 
-  async sendTrx(privateKey, recipient, amount){
-    let myAddress = accounts.privateKeyToAddress(privateKey);
-    let myHex = pubToHex(myAddress);
-    let theirHex = pubToHex(recipient);
-    let transaction = await this.getUnsignedSendTrx(myHex, theirHex, amount);
-    transaction = this.signTransaction(privateKey, transaction);
-    return await this.broadcastTransaction(transaction);
-  }
+    signTransaction(privateKey, transaction) {
+        transaction.signature = getSignature(privateKey, transaction.txID);
+        return transaction;
+    }
 
-  /******************************************************
-   ********* REQUESTING UNSIGNED TRANSACTIONS ***********
-   ******************************************************/
+    async broadcastTransaction(transaction) {
+        return (await this.fullReq("/wallet/broadcasttransaction", transaction)).data;
+    }
 
-  async getUnsignedSendTrx(from, to, amount){
-    return await this.rpcReq("/wallet/createtransaction", {
-      "to_address": to,
-      "owner_address": from,
-      "amount": amount
-    });
-  }
+    async sendTrx(privateKey, recipient, amount) {
+        let myAddress = accounts.privateKeyToAddress(privateKey);
+        let myHex = pubToHex(myAddress);
+        let theirHex = pubToHex(recipient);
+        let transaction = await this.getUnsignedSendTrx(myHex, theirHex, amount);
+        transaction = this.signTransaction(privateKey, transaction);
+        return await this.broadcastTransaction(transaction);
+    }
+
+    async getAccount(address) {
+        let addressHex = pubToHex(address);
+        return await this.solidityReq('/walletsolidity/getaccount', {
+            address: addressHex
+        });
+    }
+
+    /******************************************************
+     ********* REQUESTING UNSIGNED TRANSACTIONS ***********
+     ******************************************************/
+
+    async getUnsignedSendTrx(from, to, amount) {
+        return await this.fullReq("/wallet/createtransaction", {
+            "to_address": to,
+            "owner_address": from,
+            "amount": amount
+        });
+    }
 
 }
 
